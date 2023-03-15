ec2ba5f1d77c81ece41a1f6b9ae7ccb5ae1fc5a4
soufiyane <soufiyane.aitmoulay@gmail.com>
2023-03-15 21:23:14 +0100

    adding wowJs

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,13 +1,23 @@
 import '../assets/css/Hero.css';
-
+import WOW from 'wowjs';
+import React, { useState, useEffect } from 'react';
+import "../../node_modules/wowjs/css/libs/animate.css"
 function Hero() {
+
+    useEffect(() => {
+        const wow = new WOW.WOW({
+          live: false,
+        });
+        wow.init();
+      }, []);
+
     return (
-        <div className='hero'>
+        <div className='hero wow fadeIn' data-wow-duration="2s" data-wow-delay=".5s">
             <span>Hi, my name is</span>
             <h1>Soufiyane AitMoulay</h1>
             <h2>I build things for the web.</h2>
             <p>I'm a junior full-stack web developer, mainly interested in <span>Symfony</span> and <span>React.js</span> beside that i'm currently finishing my engineering studies in computer science.</p>
-            <button>Contact Me</button>
+           <a href='mailto:soufiyane.aitmoulay@gmail.com'>Offer for me ?</a>
         </div>
     );
 }
