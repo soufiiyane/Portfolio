0e5d6c8fd5172e84579c194077947188270768c4
soufiyane <soufiyane.aitmoulay@gmail.com>
2023-03-17 20:36:09 +0100

    site ready for deployment

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -14,24 +14,24 @@ function Experience(){
         title: "Jun 2022",
         cardTitle: "BTS in Computer Science",
         cardSubtitle:"Specialized Institute of Applied Technology",
-        cardDetailedText: "throught this program i learned so much about web development and technologies like (HTML5,CSS3,Javascript,XML,C#,.Net...) it was very fun period in my life that really shaped me into the men i am today ! to add i got graduate with a score of 95 out 100 "
+        cardDetailedText: "throught this program i learned so much about web development and technologies like (HTML5,CSS3,Javascript,XML,C#,.Net...) it was very fun period in my life that really shaped me into the men i am today ! to add i got graduate with a score of 95 out 100"
       },
       {
         title: "Aug 2022",
         cardTitle: "Back-end developer",
         cardSubtitle:"TMT company",
-        cardDetailedText: "it was really amzing experience and fun at the same time to work with people that share same passion, i worked with technologies such as WordPress,Symfony..."
+        cardDetailedText: "it was really amzing experience and fun at the same time to work with people that i share with same passion,i worked with technologies such as WordPress,Symfony"
       },
       {
         title: "Oct 2022",
         cardTitle: "Engineering Student",
         cardSubtitle:"Moroccan School of Engineering Sciences",
-        cardDetailedText: "I am presently in my third year of pursuing a degree in Computer Science through an engineering program.",
+        cardDetailedText: "I am presently in my third year of pursuing an engineering degree in Computer Science"
       }
     ];
     return(
-        <div id='experience' className='experience wow fadeIn' data-wow-duration="3s">
-          <div className='about-headline'>
+        <div id='experience' className='experience  wow fadeIn' data-wow-duration="3s">
+          <div className='about-headline specialCase'>
             <div>
                 <span>02.</span>
                 <h2>Experience</h2>
--- src/components/Socials.js
+++ src/components/Socials.js
@@ -14,10 +14,10 @@ function Socials(){
 
     return(
         <ul className='wow fadeInDown' data-wow-duration="1s">
-            <li><a href="#"><i className="fa-brands fa-github-alt"></i></a></li>
-            <li><a href="#"><i className="fa-brands fa-instagram"></i></a></li>
-            <li><a href="#"><i className="fa-brands fa-twitter"></i></a></li>
-            <li><a href="#"><i className="fa-brands fa-linkedin-in"></i></a></li>
+            <li><a target='_blank' href="https://github.com/soufiiyane"><i className="fa-brands fa-github-alt"></i></a></li>
+            <li><a target='_blank' href="https://www.linkedin.com/in/soufiiyane/"><i className="fa-brands fa-linkedin-in"></i></a></li>
+            <li><a target='_blank' href="https://www.instagram.com/soufiiyane/"><i className="fa-brands fa-instagram"></i></a></li>
+            <li><a target='_blank'  href="https://twitter.com/AitSoufiyane"><i className="fa-brands fa-twitter"></i></a></li>
             <li><span></span></li>
         </ul>
     )
