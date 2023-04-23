3244c7035eb0c9d3b0f77e46b89ef0c90dbf9243
soufiyane <soufiyane.aitmoulay@gmail.com>
2023-04-23 22:59:22 +0000

    updating

--- src/components/Work.js
+++ src/components/Work.js
@@ -27,17 +27,58 @@ function Work(){
             </div>
         <section id="projects">
             <article>
-                <div class="text">
-                    <h4>Latest Project</h4>
+                <div className="text">
+                    <p className='color'>Featured Project</p>
                     <h3>Yinas Theme</h3>
-                    <p class="blackbox">
-                        Description of the project. This should be fairly concise while
+                    <p className="blackbox">
+                        <span>Description of the project. This should be fairly concise while
                         also describing the key components that you developed or worked
                         on. It can be as long as you need it to be but should at least be
                         a few sentences long. Be sure to include specific links anywhere
-                        in the description. A link looks like
+                        in the description. A link looks like</span>
                     </p>
-                    <h4>Technologies used include:</h4>
+                    <p>Technologies used include:</p>
+                    <ul>
+                        <li>HTML</li>
+                        <li>CSS</li>
+                        <li>SVG</li>
+                    </ul>
+                    <a href='#'>Learn More</a>
+                </div>
+                <img src={yinas} alt="Screenshot of the Wall of Wonder." />
+            </article>
+            <article>
+                <div className="text">
+                    <p className='color'>Featured Project</p>
+                    <h3>Yinas Theme</h3>
+                    <p className="blackbox">
+                        <span>Description of the project. This should be fairly concise while
+                        also describing the key components that you developed or worked
+                        on. It can be as long as you need it to be but should at least be
+                        a few sentences long. Be sure to include specific links anywhere
+                        in the description. A link looks like</span>
+                    </p>
+                    <p>Technologies used include:</p>
+                    <ul>
+                        <li>HTML</li>
+                        <li>CSS</li>
+                        <li>SVG</li>
+                    </ul>
+                </div>
+                <img src={yinas} alt="Screenshot of the Wall of Wonder." />
+            </article>
+            <article>
+                <div className="text">
+                    <p className='color'>Featured Project</p>
+                    <h3>Yinas Theme</h3>
+                    <p className="blackbox">
+                    <span>Description of the project. This should be fairly concise while
+                        also describing the key components that you developed or worked
+                        on. It can be as long as you need it to be but should at least be
+                        a few sentences long. Be sure to include specific links anywhere
+                        in the description. A link looks like</span>
+                    </p>
+                    <p>Technologies used include:</p>
                     <ul>
                         <li>HTML</li>
                         <li>CSS</li>
